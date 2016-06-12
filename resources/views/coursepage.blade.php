88f32aa5d96493201e799a7fa52e209b80b1bb96
Roman Fominykh <darkromanovich@gmail.com>
2016-06-12 07:02:18 +0300

    template

--- resources/views/coursepage.blade.php
+++ resources/views/coursepage.blade.php
@@ -16,21 +16,42 @@
 <div class="container">
 <h2>About this course</h2>
 	<div class="row">
-		<div class="col-xs-8 coursetext">
-		<div class="well">
+		<div class="col-xs-8 well">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar velit vitae turpis ultricies congue. Nullam commodo faucibus tellus quis hendrerit. Donec mollis erat vel ullamcorper finibus. Aenean rhoncus lobortis augue, vitae egestas ligula auctor vitae. Morbi interdum dui at eros auctor, molestie sagittis ante volutpat. Curabitur sollicitudin purus orci, quis molestie leo condimentum sed. Suspendisse mollis tempus odio, in tempor libero consectetur ut. Nunc porttitor, ex ut semper viverra, ex mauris hendrerit massa, at lacinia lorem ipsum in diam. Nulla fermentum tellus in congue imperdiet. Praesent semper sem ante, quis aliquam arcu tristique sit amet. Proin at nisi id est convallis luctus vitae id diam. Aenean finibus consequat augue sagittis vehicula. Nulla sed efficitur nunc, efficitur porta massa. Etiam mattis consequat mauris vitae tincidunt. Praesent fringilla, neque et tempus facilisis, erat leo placerat ipsum, eget viverra libero diam sit amet nibh. Donec non elit euismod, cursus sapien facilisis, elementum metus.
 
 			Nullam a purus odio. Fusce sed massa et ligula mollis congue quis a erat. Pellentesque vitae elit nisl. Praesent ut sem vehicula, consequat libero nec, fermentum enim. Pellentesque urna nisi, tincidunt vitae sapien a, hendrerit tincidunt ex. Sed quam lorem, faucibus a est sit amet, consectetur dignissim libero. Maecenas posuere interdum dictum. Proin cursus felis eu mi consequat elementum. Vivamus vel gravida magna, ut posuere diam. Etiam faucibus mi vitae turpis finibus, vel luctus tellus lobortis. Fusce diam sapien, congue et fringilla nec, tempus fringilla libero. Vivamus ultricies quis massa at luctus. Nulla rutrum quis leo quis convallis. Sed scelerisque feugiat risus, nec pellentesque arcu faucibus ut.
 
 			Aenean vitae lectus non felis sagittis vehicula. Aenean in gravida sem. Aenean nec metus sed mauris rhoncus volutpat eget nec tellus. Nullam ut augue eu arcu laoreet sodales. Praesent ut quam luctus, dapibus tortor et, hendrerit risus. Pellentesque placerat et nisi nec. </p>
 		</div>
+		<div class="col-xs-4 well">
+		<p>Lenght - 6 weeks</p>
+		<p>Effort - 2-4 hours per week</p>
+		<p>Price - FREE Add a Verified Certificate for $49</p>
+		<p>Subject - Language</p>
+		<p>Level - Introductory</p>
 		</div>
-		<div class="col-xs-4 coursetext">
-		<p>Lenght - </p>
-		<p>Effort - </p>
-		<p>Price - </p>
-		<p>Subject - </p>
-		<p>Level - </p>
+	</div>
+	<div class="row">
+		<div class="col-xs-8 well">
+			<h2>Tutors</h2>
+			<div class="row">
+				<div class="product col-sm-3 col-md-3 col-lg-3 well">
+					<img src="/img/tutor1.jpg" alt="Tutor 1" width="155" class="img-circle">
+					<p class="text-center">Tutor 1</p>
+				</div>
+				<div class="product col-sm-3 col-md-3 col-lg-3 well">
+					<img src="/img/tutor2.jpg" alt="Tutor 2" width="155" class="img-circle">
+					<p class="text-center">Tutor 2</p>
+				</div>
+				<div class="product col-sm-3 col-md-3 col-lg-3 well">
+					<img src="/img/tutor3.jpg" alt="Tutor 3" width="155" class="img-circle">
+					<p class="text-center">Tutor 3</p>
+				</div>
+				<div class="product col-sm-3 col-md-3 col-lg-3 well">
+					<img src="/img/tutor1.jpg" alt="Tutor 1" width="155" class="img-circle">
+					<p class="text-center">Tutor 4</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
