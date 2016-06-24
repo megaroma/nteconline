7bb0bb66fed14acb9df6eed0eb1d494bd587f4f9
Dark Demon <darkromanovich@gmail.com>
2016-06-24 00:21:44 -0400

    slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -77,9 +77,13 @@
       </a>
     </div><!-- /.carousel -->
 
-<p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/p3M6_wKTSAE" frameborder="0" allowfullscreen></iframe>
-</p>
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/p3M6_wKTSAE" frameborder="0" allowfullscreen></iframe>
+    </div>
+  </div>
+</div>
 
 <h2 class="text-center">New Courses</h2>
   <div class="container">
